6665977f964eace50b3f8ec9d772df043dcf3632
kennymongali <s.mongali@gmail.com>
2025-07-02 22:50:58 +0200

    migrated

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=[('M','Male'),('F','Female')])
+    age = models.IntegerField()
+    hire_date = models.DateField()
+    # add fields like satisfaction_level, salary, turnover_flag etc.
